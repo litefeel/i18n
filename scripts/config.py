5b8026eab7562871cc073db7611ad18bbe5c3afb
litefeel <litefeel@gmail.com>
2017-09-14 17:59:45 +0800

    config path support *

--- scripts/config.py
+++ scripts/config.py
@@ -3,21 +3,40 @@
 # https://openpyxl.readthedocs.io/en/default/
 # 
 import os, os.path
+import re
 from function import readyaml
 
 
 class Sheet:
     __slots__ = ['path', 'ext', 'colNames']
-    def __init__(self, path, suffix, colNames):
+    def __init__(self, path, colNames):
         self.colNames = colNames
         self.path = path
-        if len(suffix) > 0:
-            self.path = "%s.%s" % (path, suffix)
 
     def __str__(self):
         return "path:%s colNames:%s" % (self.path, self.colNames)
     def __repr__(self):
         return self.__str__()
+
+def tryAppend(sheets, path, prefix, suffix, v):
+    if len(suffix) > 0:
+        path = "%s.%s" % (path, suffix)
+    path = os.path.join(prefix, path)
+    idx = path.find('*')
+    if -1 == idx:
+        sheets.append(Sheet(path, v))
+    else:
+        path = path.replace('\\', '/')
+        pattern = re.compile(path.replace('.', '\\.').replace('*', '.*') + '$')
+        parent = os.path.dirname(path[0:idx] )
+        # print('pattern', pattern.pattern)
+        # print('parent', parent)
+        # print('path[0:idx]', path[0:idx], idx)
+        for root, dirs, files in os.walk(parent):
+            for file in files:
+                file = '%s/%s' % (root, file)
+                if pattern.match(file) is not None:
+                    sheets.append(Sheet(file, v))
 
 def parseSheets(map, sheets = None, prefix = '', suffix = ''):
     sheets = [] if sheets is None else sheets
@@ -25,8 +44,9 @@ def parseSheets(map, sheets = None, prefix = '', suffix = ''):
         suffix = map['extension']
     for k, v in map.iteritems():
         if k != 'extension':
-            if isinstance(v, (list, tuple)):
-                sheets.append(Sheet(os.path.join(prefix, k), suffix, v))
+            if isinstance(v, (list, tuple)) or v is None:
+                tryAppend(sheets, k, prefix, suffix, v)
+                # sheets.append(Sheet(os.path.join(prefix, k), suffix, v))
             else:
                 parseSheets(v, sheets, os.path.join(prefix, k), suffix)
     return sheets
