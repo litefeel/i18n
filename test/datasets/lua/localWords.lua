2e2d40f2e3065c6f309d3a636d78547ef1bff6c3
litefeel <litefeel@gmail.com>
2017-09-15 18:10:19 +0800

    move test dataset files to test directory

---
+++ test/datasets/lua/localWords.lua
@@ -0,0 +1,11 @@
+﻿
+local localWords = {
+    ["dateString"] = "%s年%s月%s日",
+    ["dateString2"] = "%s月%s日",
+    ["timeString"] = "%s时%s分%s秒",
+    ["retryHttpTitle"] = "加载失败",
+    ["retryHttp"] = "加载失败,是否重试?",
+    ["retryString"] = "重试",
+}
+
+return localWords
