75ca20691c863a9a48b96ae88b3a1799da2a6cb0
litefeel <litefeel@gmail.com>
2018-03-31 15:42:01 +0800

    i18n

--- litefeel/i18n/__init__.py
+++ litefeel/i18n/__init__.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
-# 
+#
 from .function import *
 from .config import Config
 from . import importer
 from . import exporter
 from . import checker
+
+# Note: Keep this variable as plain string so it could be updated automatically
+#       when doing a release.
+__version__ = '1.0.0'
--- litefeel/i18n/__main__.py
+++ litefeel/i18n/__main__.py
@@ -2,83 +2,99 @@
 # -*- coding: utf-8 -*-
 # requed:
 # https://openpyxl.readthedocs.io/en/default/
-# 
+#
 # require:
 # pip install pyyaml
 # pip install polib
 # pip install openpyxl
-# 
+#
 import sys
 if sys.version_info < (3, 0):
-    print >>sys.stderr, 'current python version:%s' % sys.version
-    print >>sys.stderr, 'please use python 3'
+    print >> sys.stderr, 'current python version:%s' % sys.version
+    print >> sys.stderr, 'please use python 3'
     exit(1)
 
-from scripts import correctstdout
+from . import correctstdout
 correctstdout.correct_stdout()
 correctstdout.correct_stderr()
 
+from . import __version__
+
 import argparse
 import textwrap
-from scripts.adapters.dataset import csvadapter
-from scripts.adapters.dataset import xmladapter
-from scripts.adapters.dataset import luaadapter
-from scripts.adapters.dataset import exceladapter
-from scripts import *
-
-# version number
-VERSION = '1.0'
+from .adapters.dataset import csvadapter
+from .adapters.dataset import xmladapter
+from .adapters.dataset import luaadapter
+from .adapters.dataset import exceladapter
+from . import *
 
 
 def docheck(dirs, exts, output, adaptermap):
-    dirs = strsplit(dirs, sep = ',')
-    exts = strsplit(exts, sep = ',')
+    dirs = strsplit(dirs, sep=',')
+    exts = strsplit(exts, sep=',')
     checker.docheck(dirs, exts, output, adaptermap)
 
 
 def import_or_export_param(parser):
-    parser.add_argument('-c', '--config', default = 'config.yml',
+    parser.add_argument(
+        '-c',
+        '--config',
+        default='config.yml',
         help='config file (default: %(default)s)')
-    parser.add_argument('-r', '--root',
-        help='override rootdir of config')
+    parser.add_argument('-r', '--root', help='override rootdir of config')
 
-# -------------- main ----------------
-if __name__ == '__main__':
+
+def run(args=None):
     PROG = os.path.basename(__file__)
-    parser = argparse.ArgumentParser(usage='%(prog)s <subcommand> [options] [args]',
-        formatter_class=argparse.RawTextHelpFormatter, prog=PROG,
+    parser = argparse.ArgumentParser(
+        usage='%(prog)s <subcommand> [options] [args]',
+        formatter_class=argparse.RawTextHelpFormatter,
+        prog=PROG,
         description=textwrap.dedent("""
             export translation and import it.
             """))
 
-    parser.add_argument('-v', '--version', action='version',
-        version='%(prog)s version ' + VERSION)
-    
-    subparsers = parser.add_subparsers(title='subcommands',
-        dest='subcommand', prog=PROG)
+    parser.add_argument(
+        '-v', '--version', action='version',
+        version=__version__,
+        help='Show program version info and exit.')
+
+    subparsers = parser.add_subparsers(
+        title='subcommands', dest='subcommand', prog=PROG)
 
     # import or export
-    import_parser = subparsers.add_parser('import', aliases='i',
+    import_parser = subparsers.add_parser(
+        'import',
+        aliases='i',
         help='import translation to dataset',
         description='import translation to dataset')
-    export_parser = subparsers.add_parser('export', aliases='e',
+    export_parser = subparsers.add_parser(
+        'export',
+        aliases='e',
         help='export dataset to translation',
         description='export dataset to translation')
     import_or_export_param(import_parser)
     import_or_export_param(export_parser)
 
     # checker
-    check_parser  = subparsers.add_parser('check',
+    check_parser = subparsers.add_parser(
+        'check',
         help='automatically detect the text to be translated',
         description='automatically detect the text to be translated')
-    check_parser.add_argument('dirs',
-        help='the path for check i18n, path1,path2,path3')
-    check_parser.add_argument('-e', '--exts', default = 'xml,csv',
+    check_parser.add_argument(
+        'dirs', help='the path for check i18n, path1,path2,path3')
+    check_parser.add_argument(
+        '-e',
+        '--exts',
+        default='xml,csv',
         help='the extension for check i18n (default: %(default)s)')
-    check_parser.add_argument('-o', '--output', default = 'checker.yml',
+    check_parser.add_argument(
+        '-o',
+        '--output',
+        default='checker.yml',
         help='output filename for check i18n (default: %(default)s)')
 
-    args = parser.parse_args()
+    args = parser.parse_args(args)
 
     adaptermap = {
         "xml": xmladapter,
@@ -101,4 +117,8 @@ if __name__ == '__main__':
             importer.doimport(cfg, adaptermap)
         elif args.subcommand in ('e', 'export'):
             exporter.doexport(cfg, adaptermap)
-    
+
+
+# -------------- main ----------------
+if __name__ == '__main__':
+    run()
